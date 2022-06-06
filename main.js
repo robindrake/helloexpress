ffd5d81197a39dfdd439de3168617909b0bda125
Robyn Drake <robin.drake.041@gmail.com>
2022-06-06 03:42:28 -0500

    Updated main.js to be a true MWE(Minimum Working Example)

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 65000;
+const port = 3000;
 
 app.get("/", (req, res) => {
 	res.send("Hello Express!");
-});
-
-app.all("/secret", (req, res, next) => {
-	res.send("the secret route");
-	next();
 });
 
 app.listen(port, () => {
