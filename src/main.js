1a287ed23a251663a7f2c3321a2ded53669c1afb
Robyn Drake <robin.drake.041@gmail.com>
2022-06-09 02:45:14 -0500

    main.js compiled from typescript

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => {
-	res.send("Hello Express!");
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var app = (0, express_1["default"])();
+var port = 65000;
+app.get("/", function (req, res) {
+    res.send("Hello Typescript with Express!");
 });
-
-app.listen(port, () => {
-	console.log(`Example listening on port ${port}`);
+app.listen(port, function () {
+    console.log("Example listening on port ".concat(port));
 });
