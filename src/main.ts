5126e6b101310efbe71ec2811da845dd1b350f8d
Robyn Drake <robin.drake.041@gmail.com>
2022-06-09 02:44:23 -0500

    commit for #typescript

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app: express.Application = express();
+const port:number = 65000;
+
+app.get("/", (req, res) => {
+	res.send("Hello Typescript with Express!");
+});
+
+app.listen(port, () => {
+	console.log(`Example listening on port ${port}`);
+});
