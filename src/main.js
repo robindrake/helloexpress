7e1c3634e3999d2168be3629a478430d060c8072
Robyn Drake <robin.drake.041@gmail.com>
2022-06-09 04:44:46 -0500

    hello world done

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 "use strict";
 exports.__esModule = true;
-var express_1 = require("express");
-var app = (0, express_1["default"])();
+var express = require("express");
+var app = express();
 var port = 65000;
 app.get("/", function (req, res) {
     res.send("Hello Typescript with Express!");
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import * as express from "express";
 
 const app: express.Application = express();
 const port:number = 65000;
